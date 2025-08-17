95a81cee12f0891333ab77064c9e0728d4b79906
sdeseille <sebastien.deseille@gmail.com>
2025-08-17 22:49:22 +0200

    define minimal constant of the game

--- assets/js/gameloop.js
+++ assets/js/gameloop.js
@@ -3,3 +3,10 @@ let { init, Sprite, Text, Grid, GameLoop, track, initPointer, initKeys, onKey, B
 let { canvas, context } = init();
 initPointer();
 initKeys();
+
+// ------------ CONSTANT ------------
+var GRAV = 0.5, JUMP = -8.5;
+var GROUND_Y = Math.floor(canvas.height * 0.7); // same pos everywhere
+var DETECT_R = 110;   // Radius to detect player (IA switch to chase mode)
+var ATTACK_R = 46;    // attack radius (crouch -> dash)
+
